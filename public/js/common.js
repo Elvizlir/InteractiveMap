d1ef182c31fdcc94720f28d3918c62304a002cd4
Roman Makeev <elvizlir@gmail.com>
2019-12-14 19:51:00 +0300

    add data-square, some small changes

--- public/js/common.js
+++ public/js/common.js
@@ -4,24 +4,27 @@ function eventHandler() {
 	var infoBox = $('.s-interactive-map__info-block');
 	var infoTitle = $('.s-interactive-map__info-title');
 	var infoStatus = $('.s-interactive-map__info-status');
+	var infoSquare = $('.s-interactive-map__info-square');
 	$('.s-interactive-map__path').click(function () {
 		var title = $(this).data('plot');
+		var square = $(this).data('square');
 		infoBox.removeClass('disabled');
 		infoTitle.text("\u0423\u0447\u0430\u0441\u0442\u043E\u043A ".concat(title));
 
 		if ($(this).hasClass('sell')) {
-			infoStatus.css('background-color', '#FF7373');
+			infoStatus.css('background-color', 'rgba(13, 88, 100)');
 			infoStatus.text("\u041F\u0440\u043E\u0434\u0430\u043D");
 		} else if ($(this).hasClass('reserved')) {
-			infoStatus.css('background-color', '#64AAD0');
+			infoStatus.css('background-color', 'rgba(191, 222, 240)');
 			infoStatus.text("\u0417\u0430\u0440\u0435\u0437\u0435\u0440\u0432\u0438\u0440\u043E\u0432\u0430\u043D");
 		} else {
-			infoStatus.css('background-color', '#65E080');
+			infoStatus.css('background-color', 'rgba(243, 127, 18)');
 			infoStatus.text("\u0421\u0432\u043E\u0431\u043E\u0434\u0435\u043D");
 		}
 
 		;
-	});
+		infoSquare.text("\u041F\u043B\u043E\u0449\u0430\u0434\u044C: ".concat(square));
+	}); // console.log(square);
 }
 
 ;
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -3,25 +3,33 @@ function eventHandler() {
 	let infoBox = $('.s-interactive-map__info-block')
 	let infoTitle = $('.s-interactive-map__info-title')
 	let infoStatus = $('.s-interactive-map__info-status')
+	let infoSquare = $('.s-interactive-map__info-square')
 
 	$('.s-interactive-map__path').click(function(){
 		let title = $(this).data('plot');
+		let square = $(this).data('square');
 
 		infoBox.removeClass('disabled');
 
 		infoTitle.text(`Участок ${title}`);
 
 		if ($(this).hasClass('sell')){
-			infoStatus.css('background-color','#FF7373')
+			infoStatus.css('background-color','rgba(13, 88, 100)')
 			infoStatus.text(`Продан`);
 		} else if($(this).hasClass('reserved')){
-			infoStatus.css('background-color','#64AAD0')
+			infoStatus.css('background-color','rgba(191, 222, 240)')
 			infoStatus.text(`Зарезервирован`);
 		} else {
-			infoStatus.css('background-color','#65E080');
+			infoStatus.css('background-color','rgba(243, 127, 18)');
 			infoStatus.text(`Свободен`);
 		};
+
+		
+		infoSquare.text(`Площадь: ${square}`)
 	});
+
+	// console.log(square);
+
 
 };
 if (document.readyState !== 'loading') {
